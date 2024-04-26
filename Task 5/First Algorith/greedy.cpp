79e964d3a35ac7e81184b42db18b964fad65952f
Somaya Ayman <87386122+Somaya-Ayman@users.noreply.github.com>
2024-04-26 14:59:11 +0300

    Update greedy.cpp

--- Task 5/First Algorith/greedy.cpp
+++ Task 5/First Algorith/greedy.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-bool check_possible(int n) {
-    if (n > 0) {
-        // loop to 31 as higher numbers cannot be represented as int
-        for (int i = 2; i < 31; i++) {
-            if (n == (pow(2, i)) - 2) {
-                return true;
-            }
-        }
-        return false;
-    } else return false;
-}
+bool check_possible(int n) {if(n%4 == 0) return 1; return 0;}
 
 bool check_done(int coins[], int size) {
     // Check that half the array is filled with twos while the other half consists of zeros
@@ -31,33 +20,59 @@ void jump(int coins[], int coin1_position, int coin2_position) {
     coins[coin2_position] = 2;
 }
 
+bool move_possible(int coins[],int pos1,int pos2, int moveNum){
+        if (coins[pos1] && coins[pos2] != 1) return 0;
+        int sum = 0;
+        for (int i = pos1 + 1; i < pos2; i++) {
+           sum += coins[i];
+        }
+        if (sum == moveNum) return 1; return 0;
+}
+
 int minimum_moves(int coins[], int size) {
     if (!check_possible(size)) return -1;
-    int moves = 0;
-    if (!check_done(coins, size)) {
-        for (int i = 0; i < size; i++) {
-            if (coins[i] == 1) {
-                int sum = 0;
-                int j = 0;
-                while (sum < moves + 1) {
-                    sum += coins[i + j];
-                    j++;
+    int move = 1;
+    int count = size - 1;
+    for (int k = 0; k < ((size/4) - 1); k++){
+        while(coins[count] != 1) { count--;}
+        for (int j = count - 1 ; j > 0 ; j--){
+            if (move_possible(coins,j,count,move)){
+                jump(coins,j,count);
+                move++;
+                count--;
+
+                cout << "\nArray contents:";
+                for (int i = 0; i < size; ++i) {
+                    cout << " " << coins[i];
                 }
-                if (coins[i + j] == 1) {
-                    jump(coins, i, i + j);
-                    moves++;
-                }
+                break;
             }
         }
     }
-    return moves;
+    for(int k = 0; k < 3 * size / 4 ; k++){
+        if(coins[k] != 1) { continue;}
+        for (int j = k+1 ; j < size ; j++){
+            if (move_possible(coins,k,j,move)){
+                jump(coins,k,j);
+                move++;
+
+                cout << "\nArray contents:";
+                for (int i = 0; i < size; ++i) {
+                    cout << " " << coins[i];
+                }
+                break;
+
+            }
+        }
+    }
+    return move - 1;
 }
 
 int main() {
     int n;
     cout << "Enter the number of coins: " << endl;
     cin >> n;
-    int coins[n];
+    int *coins = new int(n);
     for (int i = 0; i < n; i++) {
         coins[i] = 1;
     }
@@ -70,9 +85,9 @@ int main() {
             cout << " " << coins[i];
         }
     } else {
-        cout << "Minimum number of moves: " << result << endl;
+        cout << "\nMinimum number of moves: " << result << endl;
         // Print the array at the end
-        cout << "Array contents:";
+        cout << "\nArray contents:";
         for (int i = 0; i < n; ++i) {
             cout << " " << coins[i];
         }
