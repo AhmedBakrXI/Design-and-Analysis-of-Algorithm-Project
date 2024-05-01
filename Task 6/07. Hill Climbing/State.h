5c9528f929d14f163c0daa9a999e7430ed193b45
Mennatallah74 <130947727+Mennatallah74@users.noreply.github.com>
2024-05-01 07:27:38 +0300

    "state class is done"

--- Task 6/07. Hill Climbing/State.h
+++ Task 6/07. Hill Climbing/State.h
@@ -9,15 +9,29 @@
 
 using namespace std;
 
+
+
 class State {
 private:
     vector<char> state;
     int heuristic;
+    bool isValidMove(int row, int col,int actualCol,int actualRow);
+    vector<State> moveChess(int position,vector<char>currentState);
+    void calculateHeuristic();
+
 
 public:
-    vector<char> getState() const { return state; }
+    State(vector<char>_state):heuristic(0){
+    	for(int idx = 0; idx< (int)_state.size();idx++)
+    	{
+    		state.push_back(_state[idx]);
+    	}
+    }
+    vector<char> getState()  const { return state; }
     int getHeuristic() const { return heuristic; }
     bool isGoal();
     vector<State> generateStates();
 };
+
+
 #endif //INC_07__HILL_CLIMBING_STATE_H
